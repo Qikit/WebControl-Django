8c08660f806ce6778cb0f4bc13e7877ab5029165
Qikit <boogarok@inbox.ru>
2025-05-28 10:55:03 +0300

    Исправление отображения изображений

--- exam_project/exam_project/urls.py
+++ exam_project/exam_project/urls.py
@@ -1,15 +1,13 @@
-# exam_project/exam_project/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Для настройки обработки медиафайлов
-from django.conf.urls.static import static # Для настройки обработки медиафайлов
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webcontrol/', include('webcontrol.urls')), # <-- Добавьте эту строку
+    path('webcontrol/', include('webcontrol.urls')),
 ]
 
-# Добавляем обработку медиафайлов для режима разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
